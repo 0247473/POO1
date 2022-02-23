e8cb1b2c10807324728004bc113a6c1917d4ff2e
0247473 <100298989+0247473@users.noreply.github.com>
2022-02-23 09:49:57 -0600

    Update main.py

--- Act1/main.py
+++ Act1/main.py
@@ -2,6 +2,7 @@ class Empleado:
     nombre: str
     puesto : str
     antiguedad: int
+    extra : int
 
     def __init__(self, nombre, puesto, antiguedad):
       self.nombre= nombre
@@ -16,7 +17,7 @@ class Empleado:
       if puesto != "Gerente":
        if antiguedad >= 1:
          
-        self.salario = self.salario + (1000*self.antiguedad)    
+        self.salario = self.salario + (1000*self.antiguedad) + self.extra   
 
     def __repr__(self):
       return "("+ str(self.nombre)+ "," + str(self.salario) + ")"
@@ -30,20 +31,19 @@ class Gerente:
       self.antiguedad= antiguedad
       print("Buenos dias jefe")
       print(Empleado(self.nombre,self.puesto,self.antiguedad))
+    
+    def salario(self, extra):
+       self.extra = extra
+       self.extra = Empleado.self.extra
       
-
-      
-
-
 class Secretaria:
-    def __init__(self, nombre, puesto, antiguedad):
+    def __init__(Empleado):
       self.nombre= nombre
       self.puesto= puesto
       self.antiguedad= antiguedad
       print("Buen trabajo, espero que el gerente no la perjudicara tanto :D")
       print(Empleado(self.nombre,self.puesto,self.antiguedad))
       
-
 class Programador:
     def __init__(self, nombre, puesto, antiguedad):
       self.nombre= nombre
@@ -52,12 +52,10 @@ class Programador:
       print("Buen trabajo, espero que no se cansara :D")
       print(Empleado(self.nombre,self.puesto,self.antiguedad))
 
-
-
-
 Gerente1 = Gerente("Miguel Perez", "Gerente", 3)
+Extra = Gerente.salario(3254)
 Secretaria1 = Secretaria("Maria Gutierrez", "Secretaria", 4)
 Programa1 = Programador("Enrique Gutierrez", "Programador", 1)
   
 
-print(Gerente1, Secretaria1, Programa1)
+print(Gerente1, Extra, Secretaria1, Programa1)
